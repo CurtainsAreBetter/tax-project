b92173a595d0e760bc535366b1b28d9176700542
Tom R <trooney1998@gmail.com>
2021-08-31 18:47:01 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name= "basic_tax_project",
     version= '0.0.1',
     description= 'Two objects meant to make working with basic taxes easier',
-    py_modules= ['tax.py'],
-    package_dir={'', 'src'}
+    py_modules= ['tax'],
+    package_dir={'': 'src'}
 )
 
